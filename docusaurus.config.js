1c7b988c41e2dbd6470d37ce3af664b7c04a0d4a
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-03 17:50:04 +0800

    Update config file
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,13 +22,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/CS203-G1/documentation.git',
+          editUrl: 'https://github.com/CS203-G1/documentation',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/CS203-G1/documentation.git',
+            'https://github.com/CS203-G1/documentation',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
